557cdb2603bcfa1b1884bf9ee60153123c1d6cd8
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-08-04 15:21:53 +0200

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:calculator/calculator.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,10 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Calculator',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: const Calculator(),
     );
   }
 }
