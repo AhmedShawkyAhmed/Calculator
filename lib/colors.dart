16557d07135a45d7d24724fa950eb845f3da7b5b
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-08-04 15:41:08 +0200

    Update

--- lib/colors.dart
+++ lib/colors.dart
@@ -4,3 +4,4 @@ import 'package:flutter/material.dart';
 Color black = Colors.black;
 Color white = Colors.white;
 Color darkBlue = const Color(0xFF003F9C);
+Color darkGrey = Colors.grey.withOpacity(0.3);
