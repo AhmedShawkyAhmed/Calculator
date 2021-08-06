3777543430e0afc03f3d0d08dd73c3cf766c61fa
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-08-06 03:52:29 +0200

    Update

--- lib/widget/drawer.dart
+++ lib/widget/drawer.dart
@@ -1,6 +1,11 @@
 import 'package:calculator/colors.dart';
+import 'package:calculator/screens/length.dart';
 import 'package:calculator/screens/scientific.dart';
+import 'package:calculator/screens/speed.dart';
 import 'package:calculator/screens/standard.dart';
+import 'package:calculator/screens/temp.dart';
+import 'package:calculator/screens/time.dart';
+import 'package:calculator/screens/weight.dart';
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
 
@@ -17,38 +22,59 @@ Widget calculatorDrawer(BuildContext context){
             ListTile(
               leading: Icon(Icons.calculate_outlined,color: white,size: 7.w,),
               title: Text('Standard',style: TextStyle(color: white,fontSize: 20.sp),),
-              onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const Standard())),
+              onTap: () => Navigator.push(
+                context,
+                PageRouteBuilder(pageBuilder: (_, __, ___) => const Standard()),
+              ),
             ),
             ListTile(
               leading: Icon(Icons.science_sharp,color: white,size: 7.w,),
               title: Text('Scientific',style: TextStyle(color: white,fontSize: 20.sp),),
-              onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const Scientific())),
+              onTap: () => Navigator.push(
+                context,
+                PageRouteBuilder(pageBuilder: (_, __, ___) => const Scientific()),
+              ),
             ),
             Text('Converter',style: TextStyle(color: yellow,fontSize: 30.sp,),),
             ListTile(
               leading: Icon(Icons.square_foot_outlined,color: white,size: 7.w,),
               title: Text('Length',style: TextStyle(color: white,fontSize: 20.sp),),
-              onTap: (){},
+              onTap: () => Navigator.push(
+                context,
+                PageRouteBuilder(pageBuilder: (_, __, ___) => const Length()),
+              ),
             ),
             ListTile(
               leading: Icon(Icons.workspaces_outline,color: white,size: 7.w,),
               title: Text('Weight',style: TextStyle(color: white,fontSize: 20.sp),),
-              onTap: (){},
+              onTap: () => Navigator.push(
+                context,
+                PageRouteBuilder(pageBuilder: (_, __, ___) => const Weight()),
+              ),
             ),
             ListTile(
               leading: Icon(Icons.thermostat_outlined,color: white,size: 7.w,),
               title: Text('Temperature',style: TextStyle(color: white,fontSize: 20.sp),),
-              onTap: (){},
+              onTap: () => Navigator.push(
+                context,
+                PageRouteBuilder(pageBuilder: (_, __, ___) => const Temperature()),
+              ),
             ),
             ListTile(
               leading: Icon(Icons.timer,color: white,size: 7.w,),
               title: Text('Time',style: TextStyle(color: white,fontSize: 20.sp),),
-              onTap: (){},
+              onTap: () => Navigator.push(
+                context,
+                PageRouteBuilder(pageBuilder: (_, __, ___) => const Time()),
+              ),
             ),
             ListTile(
               leading: Icon(Icons.speed,color: white,size: 7.w,),
               title: Text('Speed',style: TextStyle(color: white,fontSize: 20.sp),),
-              onTap: (){},
+              onTap: () => Navigator.push(
+                context,
+                PageRouteBuilder(pageBuilder: (_, __, ___) => const Speed()),
+              ),
             ),
           ],
         ),
