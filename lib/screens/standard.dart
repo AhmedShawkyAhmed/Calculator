61b5379f4dc0add6b1f7200afd3a858ac1f942ce
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-08-07 02:32:58 +0200

    Update

--- lib/screens/standard.dart
+++ lib/screens/standard.dart
@@ -180,7 +180,7 @@ class _StandardState extends State<Standard> {
       setState(() {
         number = 1;
         if (number == 1) {
-          memory = double.parse(controller.text);
+          memory = double.parse(result.text);
           assert(memory is double);
           memory += memory;
           result.text = memory.toString();
