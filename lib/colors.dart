0ebb89f0623ea3cb3a4494b7483049148f42cf97
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-08-08 19:47:45 +0200

    Update

--- lib/colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-// App Colors
-Color black = Colors.black;
-Color white = Colors.white;
-Color yellow = const Color(0xFFFFF700);
-Color darkGrey = const Color(0xFF2C2C2C);
-Color grey = const Color(0xFFB9B9B9);
---
+++ lib/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+// App Colors
+Color black = Colors.black;
+Color white = Colors.white;
+Color yellow = const Color(0xFFFFF700);
+Color darkGrey = const Color(0xFF2C2C2C);
+Color grey = const Color(0xFFB9B9B9);
+
+TextEditingController controller = TextEditingController();
+TextEditingController result = TextEditingController();
+int number = 0;
+late double a, temp1, temp2, memory, total;
+late String character;
+int? list1 = 1, list2 = 1;
+late double temp;
