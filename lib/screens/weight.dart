3f1c1ec2c33d9a84cc84d776ba71dbd515f78f01
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-08-09 02:49:27 +0200

    Done

--- lib/screens/weight.dart
+++ lib/screens/weight.dart
@@ -12,6 +12,277 @@ class Weight extends StatefulWidget {
 }
 
 class _WeightState extends State<Weight> {
+  void calculation(buttonText) {
+    if (buttonText == '0') {
+      setState(() {
+        controller.text = controller.text + buttonText.toString();
+      });
+    }
+    if (buttonText == '1') {
+      setState(() {
+        controller.text = controller.text + buttonText.toString();
+      });
+    }
+    if (buttonText == '2') {
+      setState(() {
+        controller.text = controller.text + buttonText.toString();
+      });
+    }
+    if (buttonText == '3') {
+      setState(() {
+        controller.text = controller.text + buttonText.toString();
+      });
+    }
+    if (buttonText == '4') {
+      setState(() {
+        controller.text = controller.text + buttonText.toString();
+      });
+    }
+    if (buttonText == '5') {
+      setState(() {
+        controller.text = controller.text + buttonText.toString();
+      });
+    }
+    if (buttonText == '6') {
+      setState(() {
+        controller.text = controller.text + buttonText.toString();
+      });
+    }
+    if (buttonText == '7') {
+      setState(() {
+        controller.text = controller.text + buttonText.toString();
+      });
+    }
+    if (buttonText == '8') {
+      setState(() {
+        controller.text = controller.text + buttonText.toString();
+      });
+    }
+    if (buttonText == '9') {
+      setState(() {
+        controller.text = controller.text + buttonText.toString();
+      });
+    }
+    if (buttonText == '.') {
+      if (number == 0) {
+        if (controller.text.isEmpty) {
+          setState(() {
+            controller.text = '0' + buttonText.toString();
+            number = 1;
+          });
+        } else {
+          setState(() {
+            controller.text = controller.text + buttonText.toString();
+            number = 1;
+          });
+        }
+      }
+    }
+    if (buttonText == '00') {
+      setState(() {
+        controller.text = controller.text + buttonText.toString();
+      });
+    }
+    if (buttonText == 'C') {
+      setState(() {
+        controller.text = '';
+        result.text = '';
+        number = 0;
+      });
+    }
+    if (buttonText == '⌫') {
+      setState(() {
+        number = controller.text.length;
+        controller.text = controller.text.substring(0, number - 1);
+      });
+    }
+    if (controller.text.isNotEmpty) {
+      length(list1!, list2!);
+    }
+  }
+
+  void length(int from, int to) {
+    if (controller.text.isNotEmpty) {
+      temp = double.parse(controller.text);
+      assert(temp is double);
+      if (from == 1) {
+        switch (to) {
+          case 1:
+            result.text = controller.text;
+            break;
+          case 2:
+            result.text = (temp / 10).toString();
+            break;
+          case 3:
+            result.text = (temp / 1000).toString();
+            break;
+          case 4:
+            result.text = (temp / 1000000).toString();
+            break;
+          case 5:
+            result.text = (temp * 0.00003527).toString();
+            break;
+          case 6:
+            result.text = (temp / 453592.37).toString();
+            break;
+          case 7:
+            result.text = (temp / 6350300).toString();
+            break;
+        }
+      }
+      if (from == 2) {
+        switch (to) {
+          case 1:
+            result.text = (temp * 10).toString();
+            break;
+          case 2:
+            result.text = controller.text;
+            break;
+          case 3:
+            result.text = (temp / 100).toString();
+            break;
+          case 4:
+            result.text = (temp / 100000).toString();
+            break;
+          case 5:
+            result.text = (temp * 0.00035274).toString();
+            break;
+          case 6:
+            result.text = (temp / 45359.237).toString();
+            break;
+          case 7:
+            result.text = (temp / 635029.317).toString();
+            break;
+        }
+      }
+      if (from == 3) {
+        switch (to) {
+          case 1:
+            result.text = (temp * 1000).toString();
+            break;
+          case 2:
+            result.text = (temp * 100).toString();
+            break;
+          case 3:
+            result.text = controller.text;
+            break;
+          case 4:
+            result.text = (temp / 1000).toString();
+            break;
+          case 5:
+            result.text = (temp * 0.03527396).toString();
+            break;
+          case 6:
+            result.text = (temp * 0.00220462).toString();
+            break;
+          case 7:
+            result.text = (temp * 0.00015747).toString();
+            break;
+        }
+      }
+      if (from == 4) {
+        switch (to) {
+          case 1:
+            result.text = (temp * 1000000).toString();
+            break;
+          case 2:
+            result.text = (temp * 100000).toString();
+            break;
+          case 3:
+            result.text = (temp * 1000).toString();
+            break;
+          case 4:
+            result.text = controller.text;
+            break;
+          case 5:
+            result.text = (temp * 35.2739619).toString();
+            break;
+          case 6:
+            result.text = (temp * 2.20462262).toString();
+            break;
+          case 7:
+            result.text = (temp * 0.15747304).toString();
+            break;
+        }
+      }
+      if (from == 5) {
+        switch (to) {
+          case 1:
+            result.text = (temp * 28349.5231).toString();
+            break;
+          case 2:
+            result.text = (temp * 2834.95231).toString();
+            break;
+          case 3:
+            result.text = (temp * 28.3495231).toString();
+            break;
+          case 4:
+            result.text = (temp * 0.02834952).toString();
+            break;
+          case 5:
+            result.text = controller.text;
+            break;
+          case 6:
+            result.text = (temp / 16).toString();
+            break;
+          case 7:
+            result.text = (temp / 224).toString();
+            break;
+        }
+      }
+      if (from == 6) {
+        switch (to) {
+          case 1:
+            result.text = (temp * 453592.37).toString();
+            break;
+          case 2:
+            result.text = (temp * 45359.237).toString();
+            break;
+          case 3:
+            result.text = (temp * 453.59237).toString();
+            break;
+          case 4:
+            result.text = (temp * 0.45359237).toString();
+            break;
+          case 5:
+            result.text = (temp * 16).toString();
+            break;
+          case 6:
+            result.text = controller.text;
+            break;
+          case 7:
+            result.text = (temp / 14).toString();
+            break;
+        }
+      }
+      if (from == 7) {
+        switch (to) {
+          case 1:
+            result.text = (temp * 6350300).toString();
+            break;
+          case 2:
+            result.text = (temp * 635029.317).toString();
+            break;
+          case 3:
+            result.text = (temp * 6350.29318).toString();
+            break;
+          case 4:
+            result.text = (temp * 6.35029318).toString();
+            break;
+          case 5:
+            result.text = (temp * 224).toString();
+            break;
+          case 6:
+            result.text = (temp * 14).toString();
+            break;
+          case 7:
+            result.text = controller.text;
+            break;
+        }
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,7 +309,7 @@ class _WeightState extends State<Weight> {
             Container(
               width: 100.w,
               color: black,
-              child: TextFormField(
+              child: TextField(
                 controller: controller,
                 maxLines: 1,
                 style: TextStyle(
@@ -56,7 +327,7 @@ class _WeightState extends State<Weight> {
               padding: const EdgeInsets.only(right: 25),
               child: DropdownButton(
                 hint: Text(
-                  'Select Unite',
+                  'Milligrams',
                   style: TextStyle(
                     color: grey,
                     fontSize: 25,
@@ -79,14 +350,27 @@ class _WeightState extends State<Weight> {
                     value: 3,
                   ),
                   DropdownMenuItem(
-                    child: Text('Pounds'),
+                    child: Text('Kilograms'),
                     value: 4,
+                  ),
+                  DropdownMenuItem(
+                    child: Text('Ounces'),
+                    value: 5,
+                  ),
+                  DropdownMenuItem(
+                    child: Text('Pounds'),
+                    value: 6,
+                  ),
+                  DropdownMenuItem(
+                    child: Text('Stone'),
+                    value: 7,
                   ),
                 ],
                 onChanged: (value) {
                   setState(() {
-                    list1 = value as int?;
+                    list1 = value as int;
                   });
+                  length(list1!, list2!);
                 },
               ),
             ),
@@ -95,14 +379,19 @@ class _WeightState extends State<Weight> {
               color: black,
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'Total',
+                child: TextField(
+                  controller: result,
+                  maxLines: 1,
                   style: TextStyle(
                     color: grey,
-                    fontSize: 35,
-                    fontWeight: FontWeight.w600,
+                    fontSize: 40,
                   ),
                   textAlign: TextAlign.right,
+                  readOnly: true,
+                  keyboardType: TextInputType.none,
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                  ),
                 ),
               ),
             ),
@@ -110,7 +399,7 @@ class _WeightState extends State<Weight> {
               padding: const EdgeInsets.only(right: 25),
               child: DropdownButton(
                 hint: Text(
-                  'Select Unite',
+                  'Milligrams',
                   style: TextStyle(
                     color: grey,
                     fontSize: 25,
@@ -133,14 +422,27 @@ class _WeightState extends State<Weight> {
                     value: 3,
                   ),
                   DropdownMenuItem(
-                    child: Text('Pounds'),
+                    child: Text('Kilograms'),
                     value: 4,
+                  ),
+                  DropdownMenuItem(
+                    child: Text('Ounces'),
+                    value: 5,
+                  ),
+                  DropdownMenuItem(
+                    child: Text('Pounds'),
+                    value: 6,
+                  ),
+                  DropdownMenuItem(
+                    child: Text('Stone'),
+                    value: 7,
                   ),
                 ],
                 onChanged: (value) {
                   setState(() {
-                    list2 = value as int?;
+                    list2 = value as int;
                   });
+                  length(list1!, list2!);
                 },
               ),
             ),
@@ -149,32 +451,60 @@ class _WeightState extends State<Weight> {
               children: [
                 Column(
                   children: [
-                    numButton('7', () {}, 20.w, white, darkGrey),
-                    numButton('4', () {}, 20.w, white, darkGrey),
-                    numButton('1', () {}, 20.w, white, darkGrey),
-                    numButton('.', () {}, 20.w, white, darkGrey),
+                    numButton('7', () {
+                      calculation('7');
+                    }, 20.w, white, darkGrey),
+                    numButton('4', () {
+                      calculation('4');
+                    }, 20.w, white, darkGrey),
+                    numButton('1', () {
+                      calculation('1');
+                    }, 20.w, white, darkGrey),
+                    numButton('.', () {
+                      calculation('.');
+                    }, 20.w, white, darkGrey),
                   ],
                 ),
                 Column(
                   children: [
-                    numButton('8', () {}, 20.w, white, darkGrey),
-                    numButton('5', () {}, 20.w, white, darkGrey),
-                    numButton('2', () {}, 20.w, white, darkGrey),
-                    numButton('0', () {}, 20.w, white, darkGrey),
+                    numButton('8', () {
+                      calculation('8');
+                    }, 20.w, white, darkGrey),
+                    numButton('5', () {
+                      calculation('5');
+                    }, 20.w, white, darkGrey),
+                    numButton('2', () {
+                      calculation('2');
+                    }, 20.w, white, darkGrey),
+                    numButton('0', () {
+                      calculation('0');
+                    }, 20.w, white, darkGrey),
                   ],
                 ),
                 Column(
                   children: [
-                    numButton('9', () {}, 20.w, white, darkGrey),
-                    numButton('6', () {}, 20.w, white, darkGrey),
-                    numButton('3', () {}, 20.w, white, darkGrey),
-                    numButton('00', () {}, 20.w, white, darkGrey),
+                    numButton('9', () {
+                      calculation('9');
+                    }, 20.w, white, darkGrey),
+                    numButton('6', () {
+                      calculation('6');
+                    }, 20.w, white, darkGrey),
+                    numButton('3', () {
+                      calculation('3');
+                    }, 20.w, white, darkGrey),
+                    numButton('00', () {
+                      calculation('00');
+                    }, 20.w, white, darkGrey),
                   ],
                 ),
                 Column(
                   children: [
-                    numButton('C', () {}, 43.5.w, yellow, darkGrey),
-                    numButton('⌫', () {}, 43.5.w, yellow, darkGrey),
+                    numButton('C', () {
+                      calculation('C');
+                    }, 43.5.w, yellow, darkGrey),
+                    numButton('⌫', () {
+                      calculation('⌫');
+                    }, 43.5.w, yellow, darkGrey),
                   ],
                 ),
               ],
