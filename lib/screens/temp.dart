d8c2604a109d81e9a6836485f1fa67c5aa37ab00
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-08-10 20:11:28 +0200

    Done

--- lib/screens/temp.dart
+++ lib/screens/temp.dart
@@ -107,6 +107,11 @@ class _TemperatureState extends State<Temperature> {
       setState(() {
         number = controller.text.length;
         controller.text = controller.text.substring(0, number - 1);
+        if (controller.text.isEmpty) {
+          setState(() {
+            result.text = '';
+          });
+        }
       });
     }
     if (controller.text.isNotEmpty) {
