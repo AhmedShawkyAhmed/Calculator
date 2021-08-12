820f04a90ad2a65a3c97d0af7c724f0637929ef2
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-08-12 19:08:57 +0200

    Update

--- lib/screens/scientific.dart
+++ lib/screens/scientific.dart
@@ -14,41 +14,62 @@ class Scientific extends StatefulWidget {
 class _ScientificState extends State<Scientific> {
   @override
   Widget build(BuildContext context) {
-    return RotatedBox(
-      quarterTurns: 45,
-      child: Scaffold(
-        backgroundColor: black,
-        appBar: AppBar(
-          title: Text(
-            'Scientific',
-            style: TextStyle(
-              fontSize: 25,
-              color: white,
-            ),
+    return Scaffold(
+      backgroundColor: black,
+      appBar: AppBar(
+        title: Text(
+          'Scientific',
+          style: TextStyle(
+            fontSize: 25,
+            color: white,
           ),
-          backgroundColor: black,
         ),
-        drawer: calculatorDrawer(context),
-        body: Container(
-          width: 100.w,
-          height: 100.h,
-          color: black,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Expanded(
-                flex: 1,
-                child: Container(
-                  width: 100.w,
-                  color: black,
+        backgroundColor: black,
+      ),
+      drawer: calculatorDrawer(context),
+      body: Container(
+        width: 100.w,
+        height: 100.h,
+        color: black,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Expanded(
+              flex: 1,
+              child: Container(
+                width: 100.w,
+                color: black,
+                child: TextField(
+                  controller: controller,
+                  maxLines: 1,
+                  style: TextStyle(
+                    color: white,
+                    fontSize: 45,
+                  ),
+                  textAlign: TextAlign.right,
+                  keyboardType: TextInputType.none,
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                width: 100.w,
+                color: black,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
                   child: TextField(
-                    controller: controller,
+                    controller: result,
                     maxLines: 1,
                     style: TextStyle(
-                      color: white,
-                      fontSize: 45,
+                      color: grey,
+                      fontSize: 40,
                     ),
                     textAlign: TextAlign.right,
+                    readOnly: true,
                     keyboardType: TextInputType.none,
                     decoration: const InputDecoration(
                       border: InputBorder.none,
@@ -56,96 +77,72 @@ class _ScientificState extends State<Scientific> {
                   ),
                 ),
               ),
-              Expanded(
-                flex: 1,
-                child: Container(
-                  width: 100.w,
-                  color: black,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextField(
-                      controller: result,
-                      maxLines: 1,
-                      style: TextStyle(
-                        color: grey,
-                        fontSize: 40,
-                      ),
-                      textAlign: TextAlign.right,
-                      readOnly: true,
-                      keyboardType: TextInputType.none,
-                      decoration: const InputDecoration(
-                        border: InputBorder.none,
-                      ),
-                    ),
-                  ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  children: [
+                    numButton('Sin', () {}, 5.5.h, grey, darkGrey),
+                    numButton('Cosh', () {}, 5.5.h, grey, darkGrey),
+                    numButton('Red', () {}, 5.5.h, grey, darkGrey),
+                    numButton('nPr', () {}, 5.5.h, grey, darkGrey),
+                    numButton('2^x', () {}, 5.5.h, grey, darkGrey),
+                    numButton('( - )', () {}, 5.5.h, grey, darkGrey),
+                    numButton('C', () {}, 5.5.h, yellow, darkGrey),
+                    numButton('7', () {}, 5.5.h, white, darkGrey),
+                    numButton('4', () {}, 5.5.h, white, darkGrey),
+                    numButton('1', () {}, 5.5.h, white, darkGrey),
+                    numButton('+/-', () {}, 5.5.h, white, darkGrey),
+                  ],
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Column(
-                    children: [
-                      numButton('Sin', () {}, 5.5.h, grey, darkGrey),
-                      numButton('Cosh', () {}, 5.5.h, grey, darkGrey),
-                      numButton('Red', () {}, 5.5.h, grey, darkGrey),
-                      numButton('nPr', () {}, 5.5.h, grey, darkGrey),
-                      numButton('2^x', () {}, 5.5.h, grey, darkGrey),
-                      numButton('( - )', () {}, 5.5.h, grey, darkGrey),
-                      numButton('C', () {}, 5.5.h, yellow, darkGrey),
-                      numButton('7', () {}, 5.5.h, white, darkGrey),
-                      numButton('4', () {}, 5.5.h, white, darkGrey),
-                      numButton('1', () {}, 5.5.h, white, darkGrey),
-                      numButton('+/-', () {}, 5.5.h, white, darkGrey),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      numButton('Cos', () {}, 5.5.h, grey, darkGrey),
-                      numButton('Tanh', () {}, 5.5.h, grey, darkGrey),
-                      numButton('Deg', () {}, 5.5.h, grey, darkGrey),
-                      numButton('nCr', () {}, 5.5.h, grey, darkGrey),
-                      numButton('n^x', () {}, 5.5.h, grey, darkGrey),
-                      numButton('1/x', () {}, 5.5.h, grey, darkGrey),
-                      numButton('÷', () {}, 5.5.h, yellow, darkGrey),
-                      numButton('8', () {}, 5.5.h, white, darkGrey),
-                      numButton('5', () {}, 5.5.h, white, darkGrey),
-                      numButton('2', () {}, 5.5.h, white, darkGrey),
-                      numButton('0', () {}, 5.5.h, white, darkGrey),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      numButton('Tan', () {}, 5.5.h, grey, darkGrey),
-                      numButton('mod', () {}, 5.5.h, grey, darkGrey),
-                      numButton('Nlogx', () {}, 5.5.h, grey, darkGrey),
-                      numButton('e', () {}, 5.5.h, grey, darkGrey),
-                      numButton('2√x', () {}, 5.5.h, grey, darkGrey),
-                      numButton('n!', () {}, 5.5.h, grey, darkGrey),
-                      numButton('x', () {}, 5.5.h, yellow, darkGrey),
-                      numButton('9', () {}, 5.5.h, white, darkGrey),
-                      numButton('6', () {}, 5.5.h, white, darkGrey),
-                      numButton('3', () {}, 5.5.h, white, darkGrey),
-                      numButton('.', () {}, 5.5.h, white, darkGrey),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      numButton('Sinh', () {}, 5.5.h, grey, darkGrey),
-                      numButton('log', () {}, 5.5.h, grey, darkGrey),
-                      numButton('*10^x', () {}, 5.5.h, grey, darkGrey),
-                      numButton('e^x', () {}, 5.5.h, grey, darkGrey),
-                      numButton('n√x', () {}, 5.5.h, grey, darkGrey),
-                      numButton('µ', () {}, 5.5.h, grey, darkGrey),
-                      numButton('⌫', () {}, 5.5.h, yellow, darkGrey),
-                      numButton('-', () {}, 5.5.h, yellow, darkGrey),
-                      numButton('+', () {}, 5.5.h, yellow, darkGrey),
-                      numButton('=', () {}, 13.h, black, yellow),
-                    ],
-                  ),
-                ],
-              ),
-            ],
-          ),
+                Column(
+                  children: [
+                    numButton('Cos', () {}, 5.5.h, grey, darkGrey),
+                    numButton('Tanh', () {}, 5.5.h, grey, darkGrey),
+                    numButton('Deg', () {}, 5.5.h, grey, darkGrey),
+                    numButton('nCr', () {}, 5.5.h, grey, darkGrey),
+                    numButton('n^x', () {}, 5.5.h, grey, darkGrey),
+                    numButton('1/x', () {}, 5.5.h, grey, darkGrey),
+                    numButton('÷', () {}, 5.5.h, yellow, darkGrey),
+                    numButton('8', () {}, 5.5.h, white, darkGrey),
+                    numButton('5', () {}, 5.5.h, white, darkGrey),
+                    numButton('2', () {}, 5.5.h, white, darkGrey),
+                    numButton('0', () {}, 5.5.h, white, darkGrey),
+                  ],
+                ),
+                Column(
+                  children: [
+                    numButton('Tan', () {}, 5.5.h, grey, darkGrey),
+                    numButton('mod', () {}, 5.5.h, grey, darkGrey),
+                    numButton('Nlogx', () {}, 5.5.h, grey, darkGrey),
+                    numButton('e', () {}, 5.5.h, grey, darkGrey),
+                    numButton('2√x', () {}, 5.5.h, grey, darkGrey),
+                    numButton('n!', () {}, 5.5.h, grey, darkGrey),
+                    numButton('x', () {}, 5.5.h, yellow, darkGrey),
+                    numButton('9', () {}, 5.5.h, white, darkGrey),
+                    numButton('6', () {}, 5.5.h, white, darkGrey),
+                    numButton('3', () {}, 5.5.h, white, darkGrey),
+                    numButton('.', () {}, 5.5.h, white, darkGrey),
+                  ],
+                ),
+                Column(
+                  children: [
+                    numButton('Sinh', () {}, 5.5.h, grey, darkGrey),
+                    numButton('log', () {}, 5.5.h, grey, darkGrey),
+                    numButton('*10^x', () {}, 5.5.h, grey, darkGrey),
+                    numButton('e^x', () {}, 5.5.h, grey, darkGrey),
+                    numButton('n√x', () {}, 5.5.h, grey, darkGrey),
+                    numButton('µ', () {}, 5.5.h, grey, darkGrey),
+                    numButton('⌫', () {}, 5.5.h, yellow, darkGrey),
+                    numButton('-', () {}, 5.5.h, yellow, darkGrey),
+                    numButton('+', () {}, 5.5.h, yellow, darkGrey),
+                    numButton('=', () {}, 13.h, black, yellow),
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
