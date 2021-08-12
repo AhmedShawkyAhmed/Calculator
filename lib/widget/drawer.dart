362312365f7d25d421c1a8012a78939f2ddabcc8
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-08-12 21:36:43 +0200

    Update

--- lib/widget/drawer.dart
+++ lib/widget/drawer.dart
@@ -1,7 +1,8 @@
 import 'package:calculator/constants.dart';
-import 'package:calculator/screens/calculator.dart';
 import 'package:calculator/screens/length.dart';
+import 'package:calculator/screens/scientific.dart';
 import 'package:calculator/screens/speed.dart';
+import 'package:calculator/screens/standard.dart';
 import 'package:calculator/screens/temp.dart';
 import 'package:calculator/screens/time.dart';
 import 'package:calculator/screens/weight.dart';
@@ -17,14 +18,24 @@ Widget calculatorDrawer(BuildContext context){
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Text('Calculator',style: TextStyle(color: yellow,fontSize: 30.sp,),),
             ListTile(
               leading: Icon(Icons.calculate_outlined,color: white,size: 7.w,),
-              title: Text('Calculator',style: TextStyle(color: white,fontSize: 20.sp),),
+              title: Text('Standard',style: TextStyle(color: white,fontSize: 20.sp),),
               onTap: () => Navigator.push(
                 context,
-                PageRouteBuilder(pageBuilder: (_, __, ___) => const Calculator()),
+                PageRouteBuilder(pageBuilder: (_, __, ___) => const Standard()),
               ),
             ),
+            ListTile(
+              leading: Icon(Icons.science_sharp,color: white,size: 7.w,),
+              title: Text('Scientific',style: TextStyle(color: white,fontSize: 20.sp),),
+              onTap: () => Navigator.push(
+                context,
+                PageRouteBuilder(pageBuilder: (_, __, ___) => const Scientific()),
+              ),
+            ),
+            Text('Converter',style: TextStyle(color: yellow,fontSize: 30.sp,),),
             ListTile(
               leading: Icon(Icons.square_foot_outlined,color: white,size: 7.w,),
               title: Text('Length',style: TextStyle(color: white,fontSize: 20.sp),),
